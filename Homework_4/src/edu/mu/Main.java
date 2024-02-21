911ae34ee112ffdc957c89f18cfdcffb8f6a18ea
aaron-yang799 <aaron.yang799@gmail.com>
2024-02-21 17:06:43 -0600

    edited main.java for testing parameters listed in assignment details

--- Homework_4/src/edu/mu/Main.java
+++ Homework_4/src/edu/mu/Main.java
@@ -5,13 +5,35 @@ import edu.mu.vehicle.*;
 public class Main {
 
 	public static void main(String[] args) {
-		VehicleManager instance = VehicleManager.getInstance();
-		System.out.println(instance.initializeStock());
 		
+		VehicleManager instance = VehicleManager.getInstance();
+				
+		// Read vehicle data from the vehcileList.csv file and initialize objects.
+		System.out.println(instance.initializeStock());
+		System.out.println("\n");
+
+		
+		// Display all vehicle information.
+		System.out.println("Reulst of displayAllVehicleInformation: \n");
+		instance.displayAllVehicleInformation();
+		System.out.println("\n");
+		
+		// Display all car information.
+		System.out.println("Reulst of displayAllCarInformation: \n");
+		instance.displayAllCarInformation();
+		System.out.println("\n");
+
+		
+		// Display all motorbike information.
+		System.out.println("Reulst of displayAllMotorBikeInformation: \n");
+		instance.displayAllMotorBikeInformation();
+		System.out.println("\n");
+
 		
 		System.out.println("Result of saveVehicleList: " + instance.saveVehicleList());
 		
-		System.out.println(instance.getNumberOfVehichlesByType(Car.class));
+		System.out.println("Result of getNumberOfVehiclesByType: " + instance.getNumberOfVehichlesByType(Car.class));
+		
 	}
 
 }
