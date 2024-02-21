7393567bfffedc1c5c3cfe8da2644d43aec3e9f4
aaron-yang799 <aaron.yang799@gmail.com>
2024-02-21 15:23:17 -0600

    created intializeStock method

--- Homework_4/src/edu/mu/vehicle/VehicleManager.java
+++ Homework_4/src/edu/mu/vehicle/VehicleManager.java
@@ -1,5 +1,71 @@
 package edu.mu.vehicle;
 
-public class VehicleManager {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class VehicleManager {
+	private static VehicleManager instance = null;
+	private final static String filePath = "files/vehicleList.csv";
+	private static ArrayList<Vehicle> vehicles;
+	
+	private VehicleManager() {
+		vehicles = new ArrayList<Vehicle>();
+	}
+	
+	public static VehicleManager getInstance() {
+		if(instance == null) {
+			instance = new VehicleManager();
+		}
+		return instance;
+	}
+	
+	public boolean initializeStock() {
+		try{
+			FileInputStream inputStream = new FileInputStream(filePath);
+		
+			Scanner scanner = new Scanner(inputStream);
+			scanner.nextLine();
+			while(scanner.hasNextLine()) {
+				String[] fields = scanner.nextLine().split(",");
+					switch(fields[0]) {
+						case "Truck": 
+							vehicles.add(new Truck(fields[1], fields[2], Long.parseLong(fields[3]), Double.parseDouble(fields[4]),
+									VehicleColor.valueOf(fields[5]), FuelType.valueOf(fields[6]), Double.parseDouble(fields[7]), 
+									Double.parseDouble(fields[8]), Integer.parseInt(fields[9]), Double.parseDouble(fields[10]), 
+									StartMechanism.valueOf(fields[11])));
+							break;
+						case "SUV":
+							vehicles.add(new Truck(fields[1], fields[2], Long.parseLong(fields[3]), Double.parseDouble(fields[4]),
+									VehicleColor.valueOf(fields[5]), FuelType.valueOf(fields[6]), Double.parseDouble(fields[7]), 
+									Double.parseDouble(fields[8]), Integer.parseInt(fields[9]), Double.parseDouble(fields[10]), 
+									StartMechanism.valueOf(fields[11])));						
+							break;
+						case "Car":
+							vehicles.add(new Truck(fields[1], fields[2], Long.parseLong(fields[3]), Double.parseDouble(fields[4]),
+									VehicleColor.valueOf(fields[5]), FuelType.valueOf(fields[6]), Double.parseDouble(fields[7]), 
+									Double.parseDouble(fields[8]), Integer.parseInt(fields[9]), Double.parseDouble(fields[10]), 
+									StartMechanism.valueOf(fields[11])));		
+							break;
+						case "MotorBike":
+							vehicles.add(new Truck(fields[1], fields[2], Long.parseLong(fields[3]), Double.parseDouble(fields[4]),
+									VehicleColor.valueOf(fields[5]), FuelType.valueOf(fields[6]), Double.parseDouble(fields[7]), 
+									Double.parseDouble(fields[8]), Integer.parseInt(fields[9]), Double.parseDouble(fields[10]), 
+									StartMechanism.valueOf(fields[11])));
+							break;
+						default:
+							break;
+					}
+			}
+		scanner.close();
+		inputStream.close();
+		return true;
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+			
+	}
 }
