4d20594c3aa4f7cf6ce8e8cca63cb3eef4065329
aaron-yang799 <aaron.yang799@gmail.com>
2024-02-23 02:33:15 -0600

    Commented out testing code, so Main.java reflects what was specified in the pdf provided

--- Homework_4/src/edu/mu/Main.java
+++ Homework_4/src/edu/mu/Main.java
@@ -27,38 +27,35 @@ public class Main {
 		// Display all motorbike information.
 		System.out.println("Reulst of displayAllMotorBikeInformation: \n");
 		instance.displayAllMotorBikeInformation();
-		System.out.println("\n");
-
-		
-		System.out.println("Result of saveVehicleList: " + instance.saveVehicleList());
-		
-		System.out.println("Result of getNumberOfVehiclesByType: " + instance.getNumberOfVehichlesByType(Car.class));
-		
-		System.out.println("Result of getAverageFuelEfficencyOfSUV's: " + instance.getAverageFuelEfficiencyOfSUVs(400, 10.00));
-		
-		System.out.println("\n");
-		
-		Car newCar = new Car("Ford", "Edge", (long)2010, (double)8000, VehicleColor.RED, FuelType.HYBRID, (double)30000, (double)3000, 8, (double)30, StartMechanism.KEYSTART);
-		System.out.println("Result of addVehicle: " + instance.addVehicle(newCar));
-		System.out.println("\n");
-		instance.displayAllVehicleInformation();
-		System.out.println("\n");
-
-		
-		System.out.println("Result of removeVehicle: " + instance.removeVehicle(newCar));
-		System.out.println("\n");
-		instance.displayAllVehicleInformation();
-		System.out.println("\n");
-		
-		System.out.println("Result of getVehicleWithHighestFuelEfficiency");
-		System.out.println(instance.getVehicleWithHighestFuelEfficiency(100, 5));
-		System.out.println("\n");
-		
-		System.out.println("Result ofgetVehicleWithLowestFuelEfficiency");
-		System.out.println(instance.getVehicleWithLowestFuelEfficiency(100, 5));
-
-
-
+//		System.out.println("\n");
+//
+//		
+//		System.out.println("Result of saveVehicleList: " + instance.saveVehicleList());
+//		
+//		System.out.println("Result of getNumberOfVehiclesByType: " + instance.getNumberOfVehichlesByType(Car.class));
+//		
+//		System.out.println("Result of getAverageFuelEfficencyOfSUV's: " + instance.getAverageFuelEfficiencyOfSUVs(400, 10.00));
+//		
+//		System.out.println("\n");
+//		
+//		Car newCar = new Car("Ford", "Edge", (long)2010, (double)8000, VehicleColor.RED, FuelType.HYBRID, (double)30000, (double)3000, 8, (double)30, StartMechanism.KEYSTART);
+//		System.out.println("Result of addVehicle: " + instance.addVehicle(newCar));
+//		System.out.println("\n");
+//		instance.displayAllVehicleInformation();
+//		System.out.println("\n");
+//
+//		
+//		System.out.println("Result of removeVehicle: " + instance.removeVehicle(newCar));
+//		System.out.println("\n");
+//		instance.displayAllVehicleInformation();
+//		System.out.println("\n");
+//		
+//		System.out.println("Result of getVehicleWithHighestFuelEfficiency");
+//		System.out.println(instance.getVehicleWithHighestFuelEfficiency(100, 5));
+//		System.out.println("\n");
+//		
+//		System.out.println("Result ofgetVehicleWithLowestFuelEfficiency");
+//		System.out.println(instance.getVehicleWithLowestFuelEfficiency(100, 5));
 
 	}
 
