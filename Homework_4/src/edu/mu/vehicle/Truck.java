77b0463f125256c074a9fb27c7275a7f7b1af206
zachrsnyder <zachrsnyder2004@gmail.com>
2024-02-26 20:58:48 -0600

    Added documentation to maintainence cost, fuel efficiency, and start engine methods

--- Homework_4/src/edu/mu/vehicle/Truck.java
+++ Homework_4/src/edu/mu/vehicle/Truck.java
@@ -20,17 +20,24 @@ public class Truck extends Vehicle {
 		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	//calculateMaintenanceCost:
+	//	Uses formula: maintenanceCost=distance * mass * (currentYear-modelYear) * cylinders * 0.002
+	//	to calculate maintenance cost over a distance.
 	@Override
 	public double calculateMaintenaceCost(double distance) {
 		return distance * mass * (Year.now().getValue() - modelYear) * cylinders * 0.002;
 	}
-
+	//calculateFuelEfficiency:
+	//	Uses formula: fuelEfficiency = cylinders * gasTankCapacity * fuelPrice / distance * 0.1
+	// to calculate fuel efficiency over a distance.
 	@Override
 	public double calculateFuelEfficiency(double distance, double fuelPrice) {
 		return cylinders * gasTankCapacity * fuelPrice / distance * 0.1;
 	}
-
+	
+	//startEngine:
+	//	prints vehicle start type
 	@Override
 	public void startEngine() {
 		System.out.println(startType);
